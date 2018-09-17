53c3fcf7dfe00e195206c9f8997ea6c51e84e723
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-09-17 11:39:29 -0700

    switching branches for get element by ID day6

---
+++ day3-about-me/new.js
@@ -0,0 +1,13 @@
+// PROMPTS, ALERTS AND COUNTING FOR ABOUT ME GUESSING GAME
+'use strict';
+
+var answer = prompt('Wnat is my favorite color?');
+var numbers = 0;
+
+while (number > 4) {
+  if (answer = 'red' || 'Red') {
+    alert('Yes! How\'d you know?')
+    break;
+  }
+}
+
