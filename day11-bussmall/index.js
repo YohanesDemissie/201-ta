70ee43e8e67bbf613c9fb8b5912e5246bd429296
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-09-27 12:31:38 -0700

    switching branches

--- day11-bussmall/index.js
+++ day11-bussmall/index.js
@@ -6,27 +6,33 @@ Goat.allGoats = [];
 
 //Constructor function of Goat objects
 function Goat(filePath, goatName) {
-  this.imgSource = filePath;
-  this.goatName = goatName;
-  Goat.allGoats.push(this);
+  this.imgSource = filePath; //file path
+  this.goatName = goatName; //alternative name
+  Goat.allGoats.push(this); //pushing all instants(below) of GOATS into array
 }
 
 function randomGoat() {
   var randomNumber = Math.floor(Math.random() * Goat.allGoats.length); //round down, random number (0-1) * length of array
-  imgElement.src = Goat.allGoats[randomNumber].imgSource;
-  imgElement.alt = Goat.allGoats[randomNumber].goatName;
-  console.log(imgElement.alt)
+  imgElement.src = Goat.allGoats[randomNumber].imgSource; //randomizes image by variable above
+  imgElement.alt = Goat.allGoats[randomNumber].goatName; //gives data of "image name" at index ofw
+  console.log(imgElement.alt) //prints image name in the console
 }
-
-
-// random generator to determine
-
-
 
 imgElement.addEventListener('click', randomGoat);
 
+var counterButton = document.getElementById('goat');
+var counter = 0;
+
+counterButton.addEventListener('click', function() {
+  counter++;
+  counterButton.innerHTML = counter;
+  console.log(counterButton)
+});
+
+
+
+
 // GET DOM BY ID
-new Goat('img/cruisin-goat.jpg', 'Tom-cruisin-goat');
 new Goat('img/cruisin-goat.jpg', 'Tom-cruisin-goat');
 new Goat('img/kissing-goat.jpg', 'kissing-goat');
 new Goat('img/sassy-goat.jpg', 'sassy-goat');
